b9678d51cb5cd9c4513b45f98aa70a3609284f5d
N00bcak <161533W@student.hci.edu.sg>
2019-05-12 19:00:45 +0800

    WIP assignment view
    tired and sick let me get away with this for now

---
+++ learning/coursedata.js
@@ -0,0 +1,41 @@
+const coursedata=[
+		{
+			id:"0",
+			name:"Physics",
+			teacher:"Chia Kok Pin",
+			assignments:[
+				{
+					name:"Electromagnetism 69",
+					duedate:new Date(Date.UTC(2019,4,14))
+				}
+			]
+		},
+		{
+			id:"1",
+			name:"Ingerlisc",
+			teacher:"Jeyalatha Balasingam",
+			assignments:[]
+		},
+		{
+			id:"2",
+			name:"Meth hur dur i am useless",
+			teacher: "Chua Boon Woo",
+			assignments:[
+				{
+					name:"math assignment 1"
+				},
+				{
+					name:"math assignment 2"
+				},
+				{
+					name:"low effort worksheet"
+				},
+				{
+					name:"wasting paper"
+				},
+				{
+					name:"unnecessary bullfuck"
+				}
+			]
+		}	
+	]
